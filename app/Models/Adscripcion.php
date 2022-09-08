8f27d020b78ecc5fa542beb95914056631967bd7
nirvale <nirvale@gmail.com>
2022-09-08 17:42:52 -0500

    catalogo adscripciones

---
+++ app/Models/Adscripcion.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Adscripcion extends Model
+{
+    use HasFactory;
+    protected $table = 'adscripciones';
+    protected $fillable = [
+        'CVE_USUARIO',
+        'CVE_OFICINA',
+    ];
+}
---
+++ database/migrations/2022_09_08_220456_create_adscripcions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdscripcionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adscripciones', function (Blueprint $table) {
+            $table->id();
+            $table->String('CVE_USUARIO')->references('users')->on('id')->onDelete('cascade')->onUpdate('cascade');
+            $table->String('CVE_OFICINA')->references('oficinas')->on('CVE_OFICINA')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adscripciones');
+    }
+}
