c22de198610e255c4ddfae742479aba9db5f6dc6
nirvale <nirvale@gmail.com>
2023-05-09 19:32:44 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
-    return view('welcome');
-});*/
 
-Route::get('dba', function () {
-    return view('admin.index');
+Route::get('/home', function () {
+    return view('home');
 });
 
 Route::get('password/reset', function () {
     return view('auth.passwords.email');
 });
-
-/*Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-*/
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
