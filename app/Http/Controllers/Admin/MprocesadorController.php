5af8e2c6ea5209bec117300238baf576d74d8460
nirvale <nirvale@gmail.com>
2024-02-02 19:44:53 -0600

    preparacion de catalogos para modulo de servidores y aplicacion de catalogos
    se crearon migraciones, modelos  y seeders, no impacta operacion actual

---
+++ app/Http/Controllers/Admin/MprocesadorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoremprocesadorRequest;
+use App\Http\Requests\UpdatemprocesadorRequest;
+use App\Models\mprocesador;
+
+class MprocesadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoremprocesadorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(mprocesador $mprocesador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(mprocesador $mprocesador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatemprocesadorRequest $request, mprocesador $mprocesador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(mprocesador $mprocesador)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/Admin/VirtualizadorController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorevirtualizadorRequest;
+use App\Http\Requests\UpdatevirtualizadorRequest;
+use App\Models\virtualizador;
+
+class VirtualizadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorevirtualizadorRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(virtualizador $virtualizador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(virtualizador $virtualizador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatevirtualizadorRequest $request, virtualizador $virtualizador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(virtualizador $virtualizador)
+    {
+        //
+    }
+}
